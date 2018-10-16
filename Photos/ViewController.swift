8e2c84576493cdb9c045cdab634ecb899a561d6b
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2018-10-16 14:20:01 -0500

    Intial Commit

---
+++ Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Brendan Krekeler on 10/16/18.
+//  Copyright © 2018 Brendan Krekeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
